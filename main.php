0f446ab088d899a02336ce5a945f8a8ddff945b2
xrayman021 <82471086+xrayman021@users.noreply.github.com>
2026-02-08 12:45:51 -0600

    Initial commit

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jobs Website</title>
+</head>
+<body>
+    <h1>Welcome to the Jobs Website</h1>
+    <p>Here you can work on the jobs assigned to you!</p>
+    
+</body>
+</html>
